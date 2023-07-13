86bc010d8efac5db5f00e7b59956f68c0c2ecb19
Matthew Mangiaracina <mmangiaracina99@gmail.com>
2023-07-13 19:46:10 -0400

    send it

---
+++ button-clicker.js
@@ -0,0 +1,13 @@
+console.log('javascript is connected')
+
+function turnOff(element) {
+    element.innerText = "Log Off";
+}
+
+function likes() {
+    alert('Ninja was liked')
+}
+
+function hide(element) {
+    element.remove();
+}
